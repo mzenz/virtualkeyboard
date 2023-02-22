5a21ec0e2b233fa6bbbf77b4c8d922c35b6b7b8d
Martin Z <mzenze@gmail.com>
2023-02-22 11:24:10 -0600

    repo init 🐣

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+
+.button {
+  background-color: lime;
+}
+
+.hidden {
+  opacity: 0;
+  display: fixed;
+  z-index: -100;
+}
+</style>
+
+<template>
+  <div class="container">
+    <button class="button" @click="onButtonPress">show virtual keyboard</button>
+    <input
+      ref="input"
+      class="hidden"
+      @input="onInput"
+      @keypress="onKeyPress"
+      @keydown="onKeyDown"
+      @keyup="onKeyUp"
+      @focus="onInputFocus"
+      @blur="onInputBlur"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+const input = $ref<HTMLInputElement>()
+
+const onButtonPress = () => {
+  console.debug('onButtonPress()')
+  input?.focus()
+}
+
+const onInputFocus = () => {
+  console.debug('onInputFocus()')
+}
+
+const onInputBlur = () => {
+  console.debug('onInputBlur()')
+}
+
+const onInput = (e: InputEvent) => {
+  console.debug('onInput:')
+  console.debug(e.data)
+}
+
+const onKeyDown = (e: KeyboardEvent) => {
+  console.debug('onKeyDown:')
+  console.debug(e)
+  console.debug(e.keyCode)
+}
+
+const onKeyPress = (e: KeyboardEvent) => {
+  console.debug('onKeyPress:')
+  console.debug(e)
+  console.debug(e.keyCode)
+}
+
+const onKeyUp = (e: KeyboardEvent) => {
+  console.debug('onKeyUp:')
+  console.debug(e)
+  console.debug(e.keyCode)
+}
+</script>
---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    Vue({
+      reactivityTransform: true
+    })
+  ],
+})
